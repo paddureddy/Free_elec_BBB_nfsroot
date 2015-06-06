5d580dd8474cde2792cf206c48ae5c65bbaf26cd
paddureddy <paddu.nit03@gmail.com>
2015-06-06 18:09:35 +0530

    first commit

---
+++ root/nunchuk/nunchuk.c
@@ -0,0 +1,11 @@
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/i2c.h>
+
+/* Add your code here */
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR(PADMKAAR REDDY THATIKONDA);
+MODULE_VERSION(1.0);
+
+
